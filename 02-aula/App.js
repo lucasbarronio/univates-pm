81047f003d6b97f0a03bee5f1c0fd9431a6710cd
Lucas <lucas.barronio@aztecweb.net>
2026-08-07 21:25:53 -0300

    Aula 02 - 07/08/26 - Componentes

--- 02-aula/App.js
+++ 02-aula/App.js
@@ -1,11 +1,39 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { Image, Pressable, StyleSheet, Switch, Text, TextInput, TouchableOpacity, View } from 'react-native';
 
 export default function App() {
+  const [isEnabled, setIsEnabled] = useState(false);
+  const toggleSwitch = () => setIsEnabled(previousState => !previousState);
+
   return (
+
     <View style={styles.container}>
-      <Text style={styles.text}>Open up App.js to start working on your app!</Text>
+      <Text style={styles.text}>Aula 2 - Componentes!</Text>
       <StatusBar style="auto" />
+
+      <Image
+        style={styles.logo}
+        source={require('./assets/favicon.png')}
+      />
+
+      <TextInput
+        style={styles.input}
+        value=''
+        placeholder="placeholder"
+      />
+
+      <Switch
+        trackColor={{ false: '#767577', true: '#81b0ff' }}
+        thumbColor={isEnabled ? '#f5dd4b' : '#f4f3f4'}
+        ios_backgroundColor="#3e3e3e"
+        onValueChange={toggleSwitch}
+        value={isEnabled}
+      />
+
+      <TouchableOpacity style={styles.button}>
+        <Text>Press Here</Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -13,11 +41,24 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#2b2929',
+    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    gap: '100',
   },
-  text: {
-    color: '#fbf4f4',
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
   },
+  logo: {
+    width: 64,
+    height: 64,
+  },
+  button: {
+    backgroundColor: '#b6a0a0',
+    paddingHorizontal: '22',
+    paddingVertical: '14',
+  }
 });
